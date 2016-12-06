2171b9ef48661da11781f80f991ff07f3f912dab
Joshua Haupt <jhaupt@siue.edu>
2016-12-06 13:36:52 +0000

    unique querry 2

--- project6/index.php
+++ project6/index.php
@@ -44,21 +44,7 @@ catch(PDOException $errmessage)
     }
 
 
-//First, start off the HTTP that creates a table of returned results
 
-echo "<table style='border: solid 1px black'>";
-
-echo "<p>Number of Reports filed by an Officer</p>";
-echo "<br>";
-echo "<p>SELECT COUNT(File_ReportNumber), FirstName, LastName
-
-FROM PoliceOfficer, FiledBy
-
-WHERE File_DateGraduated = DateGraduated AND File_BadgeNumber = BadgeNumber
-
-GROUP BY FirstName, LastName;</p>";
-
-echo "<tr><th>Number of Reports</th><th>Firstname</th><th>Lastname</th></tr>";
 
 
 //This essentially iterates through a tuple's fields and puts them in a single table row
@@ -113,7 +99,7 @@ class TupleResult extends RecursiveIteratorIterator {
 
 //First, use the Prepare() function to store the SELECT query for use with our database
 
-$query = $connection->prepare("SELECT COUNT(File_ReportNumber), FirstName, LastName
+$UniqueQuery1 = $connection->prepare("SELECT COUNT(File_ReportNumber), FirstName, LastName
 
 FROM PoliceOfficer, FiledBy
 
@@ -121,23 +107,49 @@ WHERE File_DateGraduated = DateGraduated AND File_BadgeNumber = BadgeNumber
 
 GROUP BY FirstName, LastName;");
 
+//Unique Querry 2
+$UniqueQuery2 = $connection->prepare("SELECT COUNT(DateGraduated), DateGraduated
+
+FROM PoliceOfficer NATURAL JOIN PoliceDepartment
+
+GROUP BY BadgeNumber;");
 
 
-//Execute the query
 
-$query->execute();
+//Execute the queries
+
+$UniqueQuery1->execute();
+
+$UniqueQuery2->execute();
 
 
 
 //Create a results array and then extract + format the returned data from the execute() statement
 
-$results = $query->setFetchMode(PDO::FETCH_ASSOC);
+$results = $UniqueQuery1->setFetchMode(PDO::FETCH_ASSOC);
 
+//Table 1
+
+//First, start off the HTTP that creates a table of returned results
+
+echo "<table style='border: solid 1px black'>";
+
+echo "<p>Number of Reports filed by an Officer</p>";
+echo "<br>";
+echo "<p>SELECT COUNT(File_ReportNumber), FirstName, LastName
+
+FROM PoliceOfficer, FiledBy
+
+WHERE File_DateGraduated = DateGraduated AND File_BadgeNumber = BadgeNumber
+
+GROUP BY FirstName, LastName;</p>";
+
+echo "<tr><th>Number of Reports</th><th>Firstname</th><th>Lastname</th></tr>";
 
 
 //Extract the results using the TableRows class
 
-foreach(new TupleResult(new RecursiveArrayIterator($query->fetchAll())) as $key=>$value)
+foreach(new TupleResult(new RecursiveArrayIterator($UniqueQuery1->fetchAll())) as $key=>$value)
 
 {
 
@@ -145,12 +157,54 @@ foreach(new TupleResult(new RecursiveArrayIterator($query->fetchAll())) as $key=
 
 }
 
+//END Table
+echo "</table>";
+
+echo "<br></br>";
+
+
+//For Unique Query 2: Create a results array and then extract + format the returned data from the execute() statement
+
+$results2 = $UniqueQuery2->setFetchMode(PDO::FETCH_ASSOC);
+
+
+//Table2
+
+echo "<table style='border: solid 1px black'>";
+
+echo "<p>Number of Officers that graduated on a given day</p>";
+echo "<br>";
+echo "<p>SELECT COUNT(DateGraduated), DateGraduated
+
+FROM PoliceOfficer NATURAL JOIN PoliceDepartment
+
+GROUP BY BadgeNumber;</p>";
+
+echo "<tr><th>Number of Police Officers</th><th>Date Graduated</th></tr>";
+
+//Extract the results using the TableRows class
+
+foreach(new TupleResult(new RecursiveArrayIterator($UniqueQuery2->fetchAll())) as $key=>$value)
+
+{
+
+    echo $value;
+
+}
+
+//END Table
+echo "</table>";
+
+echo "<br></br>";
+
 
 
 //Close connection and wrap up the returned table of results
 
 $connection = null;
 
-echo "</table>";
+//echo "</table>";
+
+//echo "<br>";
 
 ?>
