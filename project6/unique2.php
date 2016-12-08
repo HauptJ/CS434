4ada5f286c653717d2729d005569f7745e9b5574
Joshua Haupt <jhaupt@siue.edu>
2016-12-08 05:10:10 +0000

    unique query 2 works

---
+++ project6/unique2.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<h1></h1>
+
+<p><b>Data Format:</b></p>
+<p>Enter a Precinct number as an integer from 1 to 21.</p>
+
+
+<form action="unique2Results.php" method="post">
+Precinct Number: <input type="text" name="PrecinctNumber"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
